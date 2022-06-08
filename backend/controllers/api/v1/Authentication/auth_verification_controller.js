ef9c02d01ce5f42d36bfa4d91c752d3fb86a0a17
root <maryamasadi586@gmail.com>
2022-06-09 00:39:39 +0430

    180301

---
+++ backend/controllers/api/v1/Authentication/auth_verification_controller.js
@@ -0,0 +1,41 @@
+const controller = require(`${config.path.controller}/controller`);
+const jwt = require('jsonwebtoken');
+
+module.exports = new class VerificationController extends controller {
+
+   verify (req, res) {
+      const { token } = req.params
+      
+      // Check we have an id
+      if (!token) {
+         return res.status(422).send({ 
+               message: "Missing Token" 
+         });
+      }
+
+      // Step 1 -  Verify the token from the URL
+      let payload = jwt.verify( token, process.env.USER_VERIFICATION_TOKEN_SECRET );
+      
+      try{
+         // Step 2 - Find user with matching ID
+         this.model.user.findOne({ _id: payload.ID }, (err, user) => {
+            if (err) throw err;
+            if (user && !user.email_verification.verified ) {
+               // Step 3 - Update user verification status to true
+               user.email_verification.verified = true;
+               user.save();
+               return res.status(200).send({
+                  message: "Account Verified"
+               });
+            } else {
+               return res.status(404).send({
+                  message: "User does not  exists" 
+               });
+            }
+         });
+      } catch (err) {
+         return res.status(500).send(err);
+      }
+   }
+
+}
